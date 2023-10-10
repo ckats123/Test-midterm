b6e7ac103680633691ef67b674330db9f26e6f03
Sophia Katsuno <32723314+ckats123@users.noreply.github.com>
2023-10-10 12:22:44 -0700

    Initialize maintenance/sass-update

---
+++ server.js
@@ -0,0 +1,57 @@
+// load .env data into process.env
+require("dotenv").config();
+
+// Web server config
+const PORT = process.env.PORT || 8080;
+const sassMiddleware = require("./lib/sass-middleware");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+
+// Load the logger first so all (static) HTTP requests are logged to STDOUT
+// 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  "/styles",
+  sassMiddleware({
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
+    isSass: false, // false => scss, true => sass
+  })
+);
+
+app.use(express.static("public"));
+
+// Separated Routes for each Resource
+// Note: Feel free to replace the example routes below with your own
+const usersRoutes = require("./routes/users");
+const widgetsRoutes = require("./routes/widgets");
+
+// Mount all resource routes
+// Note: Feel free to replace the example routes below with your own
+app.use("/api/users", usersRoutes(db));
+app.use("/api/widgets", widgetsRoutes(db));
+// Note: mount other resources here, using the same pattern above
+
+// Home page
+// Warning: avoid creating more routes in this file!
+// Separate them into separate routes files (see above).
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
