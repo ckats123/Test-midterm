07df8ed4691de9c919d430f87be1a92f0ef75048
Kenny Lee <kenny.lee927@gmail.com>
2023-10-15 02:31:18 -0400

    write temporary search function for search bar

--- db/queries/listings.js
+++ db/queries/listings.js
@@ -16,4 +16,11 @@ const getListingById = (id) => {
   });
 };
 
-module.exports = { getAllListings, getListingById };
+const getListingSearchResults = (term) => {
+  query = `SELECT * FROM listings JOIN users ON seller_id = users.id JOIN sellers_info ON sellers_info.user_id = users.id WHERE title ILIKE '%${term}%' LIMIT 12`;
+  return db.query(query).then((data) => {
+    return data.rows;
+  })
+}
+
+module.exports = { getAllListings, getListingById, getListingSearchResults };
