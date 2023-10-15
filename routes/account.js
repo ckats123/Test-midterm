63e3d8c5cc4a58fd530381037c78eddbcc585d22
Kenny Lee <kenny.lee927@gmail.com>
2023-10-15 08:18:34 -0400

    add seller info and seller listings to account page

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,23 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const users = require("../db/queries/users");
+const listings = require("../db/queries/listings");
 
-router.get('/', (req, res) => {
-  res.render('account');
+router.get("/", (req, res) => {
+  users
+    .getSellerAccountInfo(req.cookies["email"])
+    .then((data) => {
+      const userInfo = data.rows[0];
+      console.log(userInfo);
+      listings.getListingsBySeller(userInfo["id"]).then((myListings) => {
+        res.render("account", { userInfo, myListings });
+      });
+    })
+    .catch((error) => {
+      // If user not found (not logged in), redirect to log in
+      console.log(error);
+      res.redirect("/login");
+    });
 });
 
 module.exports = router;
