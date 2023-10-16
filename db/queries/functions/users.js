fa9e124bb50d7aa4f16a0d29e4f019621eb01b63
Sophia <chiaki.katsuno@live.ca>
2023-10-16 13:51:28 -0700

    test uncomment of users.js file

--- db/queries/functions/users.js
+++ db/queries/functions/users.js
@@ -1,32 +1,32 @@
-// const db = require("../../connection");
+const db = require("../../connection");
 
-// function handleQueryError(error) {
-//   console.error("Database query error:", error);
-//   throw error;
-// }
+function handleQueryError(error) {
+  console.error("Database query error:", error);
+  throw error;
+}
 
-// const getUsers = () => {
-//   return db
-//     .query("SELECT * FROM users;")
-//     .then((data) => data.rows)
-//     .catch(handleQueryError);
-// };
+const getUsers = () => {
+  return db
+    .query("SELECT * FROM users;")
+    .then((data) => data.rows)
+    .catch(handleQueryError);
+};
 
-// const getUserPassword = (email) => {
-//   return db
-//     .query("SELECT email, password FROM users WHERE email = $1;", [email])
-//     .then((data) => data.rows[0])
-//     .catch(handleQueryError);
-// };
+const getUserPassword = (email) => {
+  return db
+    .query("SELECT email, password FROM users WHERE email = $1;", [email])
+    .then((data) => data.rows[0])
+    .catch(handleQueryError);
+};
 
-// const getSellerAccountInfo = (email) => {
-//   return db
-//     .query(
-//       "SELECT * FROM users JOIN sellers_info ON users.id = sellers_info.user_id WHERE email = $1;",
-//       [email],
-//     )
-//     .then((data) => data.rows)
-//     .catch(handleQueryError);
-// };
+const getSellerAccountInfo = (email) => {
+  return db
+    .query(
+      "SELECT * FROM users JOIN sellers_info ON users.id = sellers_info.user_id WHERE email = $1;",
+      [email],
+    )
+    .then((data) => data.rows)
+    .catch(handleQueryError);
+};
 
-// module.exports = { getUsers, getUserPassword, getSellerAccountInfo };
+module.exports = { getUsers, getUserPassword, getSellerAccountInfo };
