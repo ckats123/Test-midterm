a61e2129cb5c3006397c8c71d2106479ccf40f7c
Sophia <chiaki.katsuno@live.ca>
2023-10-15 23:43:14 -0700

    getSellerAccountInfo.js

---
+++ db/queries/getSellerAccountInfo.js
@@ -0,0 +1,15 @@
+// getSellerAccountInfo.js
+
+const db = require("../connection");
+
+const getSellerAccountInfo = (sellerid) => {
+  return db
+    .query(
+      "SELECT SELECT users.name, users.email, users.is_seller, sellers_info.photo_url, sellers_info.location FROM users JOIN sellers_info ON users.user_id = sellers_info.seller_id WHERE users.is_seller = true AND users.user_id = $1;",
+      [sellerid],
+    )
+    .then((data) => data.rows[0])
+    .catch(handleQueryError);
+};
+
+module.exports = { getSellerAccountInfo };
