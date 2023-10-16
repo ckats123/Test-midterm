7e156c00f87e1045c15c10dc913e622a2181f55a
Sophia <chiaki.katsuno@live.ca>
2023-10-16 16:13:43 -0700

    adjusted function of listings.js

---
+++ db/queries/listings.js
@@ -0,0 +1,35 @@
+// listings.js
+
+const db = require("../connection");
+
+const getAllListings = () => {
+  const query = `
+    SELECT * FROM listings 
+    JOIN users on seller_id = users.id 
+    JOIN sellers_info ON sellers_info.user_id = users.id 
+    LIMIT 12`;
+  return db.query(query).then((data) => {
+    return data.rows;
+  });
+};
+
+const getListingById = (id) => {
+  const query = "SELECT * FROM listings WHERE id=$1";
+  return db.query(query, [id]).then((data) => {
+    return data.rows;
+  });
+};
+
+const getListingSearchResults = (term) => {
+  query = `
+    SELECT * FROM listings 
+    JOIN users ON seller_id = users.id 
+    JOIN sellers_info ON sellers_info.user_id = users.id 
+    WHERE title ILIKE '%${term}%' 
+    LIMIT 12`;
+  return db.query(query).then((data) => {
+    return data.rows;
+  });
+};
+
+module.exports = { getAllListings, getListingById, getListingSearchResults };
