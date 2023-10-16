a0277fda40c5b6690db57da5445d884f8356e2ec
Emily McMahon <emilyemcmahonn@gmail.com>
2023-10-16 13:35:03 -0700

    Revert "Server"

--- db/queries/listings.js
+++ db/queries/listings.js
@@ -1,4 +1,4 @@
-const db = require("../../connection");
+const db = require("../connection");
 
 const getAllListings = () => {
   // This is a sample query
--- db/queries/users.js
+++ db/queries/users.js
@@ -1,4 +1,4 @@
-const db = require("../../connection");
+const db = require("../connection");
 
 
 function handleQueryError(error) {
