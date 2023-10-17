c34f60aba5ac1dc872bbc4a02d3965149fd7156c
Kenny Lee <kenny.lee927@gmail.com>
2023-10-17 03:54:32 -0400

    write auth check for get and post, write post route for new listings

--- routes/create-listing.js
+++ routes/create-listing.js
@@ -1,8 +1,25 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const listings = require("../db/queries/functions/listings");
 
-router.get('/', (req, res) => {
-  res.render('create-listing');
+router.get("/", (req, res) => {
+  if (req.cookies["email"] && req.cookies["is_seller"]) {
+    res.render("create-listing");
+  } else {
+    res.status(401).send("You are not logged in or not a seller");
+  }
+});
+
+router.post("/", (req, res) => {
+  if (req.cookies["email"] && req.cookies["is_seller"]) {
+    listings.createNewListing(req.cookies["user_id"], req.body.title, req.body.price, req.body.description)
+    .then ((data) => {
+      console.log("Successfully inserted new listing entry. Redirecting...")
+      res.redirect('/account')
+    })
+  } else {
+    res.status(401).send("You are not logged in or not a seller");
+  }
 });
 
 module.exports = router;
