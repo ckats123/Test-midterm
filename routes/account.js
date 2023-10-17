bb181259de4fcd0087ee6cdd04bee28467a8f049
Kenny Lee <kenny.lee927@gmail.com>
2023-10-17 01:17:52 -0400

    split rendering data logic into buyer / seller

--- routes/account.js
+++ routes/account.js
@@ -6,34 +6,56 @@ const transactions = require("../db/queries/functions/transactionLog");
 const reviews = require("../db/queries/functions/reviews");
 
 router.get("/", (req, res) => {
-  users
-    .getSellerAccountInfo(req.cookies["email"])
-    .then((data) => {
-      const userInfo = data[0];
-      listings.getListingsBySeller(userInfo["id"]).then((myListings) => {
-        transactions.getSellerTransactionLog(userInfo.id).then((mySalesLog) => {
-          reviews
-            .getReviewsForMultipleTransactions(mySalesLog)
-            .then((myReviews) => {
-              console.log(mySalesLog);
-              console.log(myReviews);
-              const isSeller = req.cookies["is_seller"];
-              res.render("account", {
-                userInfo,
-                myListings,
-                mySalesLog,
-                myReviews,
-                isSeller
-              });
+  // This is all logic for seller users. A cursed async nightmare
+  if (req.cookies["is_seller"]) {
+    users
+      .getSellerAccountInfo(req.cookies["email"])
+      .then((data) => {
+        const userInfo = data[0];
+        listings.getListingsBySeller(userInfo["id"]).then((myListings) => {
+          transactions
+            .getSellerTransactionLog(userInfo.id)
+            .then((mySalesLog) => {
+              transactions
+                .getBuyerTransactionLog(userInfo.id)
+                .then((myBuyLog) => {
+                  reviews
+                    .getReviewsForMultipleTransactions(mySalesLog)
+                    .then((myReviews) => {
+                      const isSeller = req.cookies["is_seller"];
+                      res.render("account", {
+                        userInfo,
+                        myListings,
+                        mySalesLog,
+                        myBuyLog,
+                        myReviews,
+                        isSeller,
+                      });
+                    });
+                });
             });
         });
+      })
+      .catch((error) => {
+        // If user not found (not logged in), redirect to log in
+        console.log(error);
+        res.redirect("/login");
+      });
+  } else {
+    // This is all the logic for if the user is only a buyer
+    const isSeller = req.cookies["is_seller"];
+    users.getUserByEmail(req.cookies["email"]).then((userInfo) => {
+      transactions.getBuyerTransactionLog(userInfo.id).then((myBuyLog) => {
+        console.log(myBuyLog);
+        res.render("account", { userInfo, isSeller, myBuyLog });
       });
     })
     .catch((error) => {
       // If user not found (not logged in), redirect to log in
       console.log(error);
       res.redirect("/login");
-    });
+    });;
+  }
 });
 
 module.exports = router;
