615aa6a6f34de4aaae8dfad1e4d352e03da3bb8a
Kenny Lee <kenny.lee927@gmail.com>
2023-10-18 00:09:30 -0400

    write POST route logic for creating new buyer or seller user

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,36 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const users = require("../db/queries/functions/users");
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+router.get("/", (req, res) => {
+  res.render("users");
+});
+
+router.post("/", (req, res) => {
+  const submittedDetails = req.body;
+  if (!submittedDetails.is_seller) {
+    // Buyer logic
+    users.addBuyerUser(submittedDetails).then((createdUser) => {
+      console.log("Created Buyer User:", createdUser);
+    })
+    .catch((error) => {
+      console.error("Error creating buyer user:", error);
+      res.status(500).send("An error occurred while creating the buyer user.");
+    });
+  } else {
+    // Seller logic
+    users.addSellerUser(submittedDetails).then((createdUser) => {
+      console.log("Created Seller User:", createdUser);
+    })
+    .catch((error) => {
+      console.error("Error creating seller user:", error);
+      res.status(500).send("An error occurred while creating the seller user.");
+    });
+  }
+  res.redirect("/login");
+});
 
 module.exports = router;
