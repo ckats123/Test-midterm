55e6b192744a3b584acdd6b8312fb3dab5698854
Kenny Lee <kenny.lee927@gmail.com>
2023-10-19 02:50:05 -0400

    write function to post a new transaction to the log

---
+++ db/queries/functions/transactions.js
@@ -0,0 +1,25 @@
+const db = require("../../connection");
+
+const postNewTransaction = (
+  listing_id,
+  buyer_id,
+  purchase_date,
+  final_cost
+) => {
+  console.log(listing_id, buyer_id, purchase_date, final_cost);
+  const query = `
+  INSERT INTO transactions (listing_id, buyer_id, purchase_date, final_cost)
+  VALUES ($1, $2, $3, $4)
+  RETURNING *;
+  `;
+  return db
+    .query(query, [listing_id, buyer_id, purchase_date, final_cost])
+    .then((result) => {
+      const createdTransaction = result.rows[0];
+      return createdTransaction;
+    });
+};
+
+module.exports = {
+  postNewTransaction,
+};
