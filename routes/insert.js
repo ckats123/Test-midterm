19a2b23d8ecd0918f0a9463618a9709e2cdd5256
Kenny Lee <kenny.lee927@gmail.com>
2023-10-19 21:41:28 -0400

    nest isListingFavorites in then

--- routes/insert.js
+++ routes/insert.js
@@ -2,33 +2,38 @@ const express = require("express");
 const router = express.Router();
 const users = require("../db/queries/functions/users");
 
-router.post("/:listingId", (req, res) => {
-console.log('testing', req.cookies['user_id'], req.params.listingId)
-  users.addFavorite(req.cookies["user_id"], req.params.listingId)
-  .then((favorite) => {
-    console.log("Successfully inserted new favorite entry.")
-    })
-    .catch((error) => {
-    res.status(500).json({ error: 'Failed to add to favorites.' });
-  });
-});
-
 // router.post("/:listingId", (req, res) => {
-//   // Check if the listing is already in the user's favorites
-//   if (users.isListingInFavorites(req.cookies['user_id'], req.params.listingId)) {
-//     console.log('testing', req.cookies['user_id'], req.params.listingId);
-//     users.addFavorite(req.cookies["user_id"], req.params.listingId)
-//       .then((favorite) => {
-//         console.log("Successfully inserted new favorite entry.");
-//       })
-//       .catch((error) => {
-//         console.log("Failed to add to favorites.")
-//       });
-//   } else {
-//     console.log('Listing already in favorites!');
-//   }
+// console.log('testing', req.cookies['user_id'], req.params.listingId)
+//   users.addFavorite(req.cookies["user_id"], req.params.listingId)
+//   .then((favorite) => {
+//     console.log("Successfully inserted new favorite entry.")
+//     })
+//     .catch((error) => {
+//     res.status(500).json({ error: 'Failed to add to favorites.' });
+//   });
 // });
 
+router.post("/:listingId", (req, res) => {
+  // Check if the listing is already in the user's favorites
+  users.isListingInFavorites(req.cookies['user_id'], req.params.listingId)
+    .then(isntInFavorites => {
+      if (isntInFavorites) {
+        console.log('testing', req.cookies['user_id'], req.params.listingId);
+        users.addFavorite(req.cookies["user_id"], req.params.listingId)
+          .then((favorite) => {
+            console.log("Successfully inserted new favorite entry.");
+          })
+          .catch((error) => {
+            console.log("Failed to add to favorites.");
+          });
+      } else {
+        console.log('Listing already in favorites!');
+      }
+    })
+    .catch(error => {
+      console.log("Error checking if the listing is in favorites:", error);
+    });
+});
 
 
 
