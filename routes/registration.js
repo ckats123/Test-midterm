800595b6d5bff8519d1a9678e2a175b392b4169f
Sophia Katsuno <32723314+ckats123@users.noreply.github.com>
2023-10-20 23:36:10 -0700

    Delete routes/registration.js

--- routes/registration.js
+++
@@ -1,9 +0,0 @@
-// registration.js
-
-const express = require("express");
-const router = express.Router();
-const users = require("../db/queries/functions/users");
-
-router.get("/", (req, res) => {
-  res.render("registration");
-});
